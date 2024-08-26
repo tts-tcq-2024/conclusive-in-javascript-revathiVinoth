388c9893cde5f52a299ae12c930955d7bc543492
revathiVinoth <revzz.1993@gmail.com>
2024-08-26 13:38:46 +0530

    test cases

--- test/typewisealert.test.js
+++ test/typewisealert.test.js
@@ -1,6 +1,42 @@
 const alerts = require('../src/typewisealert');
-const {expect} = require('chai');
+const { expect } = require('chai');
+const { sendToController, sendToEmail } = require('../src/sendAlerts');
 
-it('infers a value lower than the minimum as TOO_LOW', () => {
-  expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
+describe('Test -Infer alerts', () => {
+  it('infers a value lower than the minimum as TOO_LOW', () => {
+    expect(alerts.inferBreach(20, 50, 100)).equals('TOO_LOW');
+  });
+  it('infers a value lower than the minimum as TOO_HIGH', () => {
+    expect(alerts.inferBreach(110, 50, 100)).equals('TOO_HIGH');
+  });
+  it('infers a value lower than the minimum as TOO_HIGH', () => {
+    expect(alerts.inferBreach(60, 50, 100)).equals('NORMAL');
+  });
+});
+
+describe('Test -classifyTemperatureBreach alerts', () => {
+  it('infers a value based on classification of coolingtype PASSIVE_COOLING', () => {
+    expect(alerts.classifyTemperatureBreach('PASSIVE_COOLING', 34)).equals('NORMAL');
+  });
+  it('infers a value based on classification of coolingtype HI_ACTIVE_COOLING', () => {
+    expect(alerts.classifyTemperatureBreach('HI_ACTIVE_COOLING', -1)).equals('TOO_LOW');
+  });
+  it('infers a value based on classification of coolingtype MED_ACTIVE_COOLING', () => {
+    expect(alerts.classifyTemperatureBreach('MED_ACTIVE_COOLING', 41)).equals('TOO_HIGH');
+  });
+});
+
+describe('Test -sendAlerts', () => {
+  it('infers a value based on classification of coolingtype PASSIVE_COOLING and send alerts to controller', () => {
+    alerts.checkAndAlert('TO_CONTROLLER', { 'coolingType': 'PASSIVE_COOLING' }, 34)
+    expect(sendToController.calledOnce)
+  });
+  it('infers a value based on classification of coolingtype HI_ACTIVE_COOLING and send alerts to email', () => {
+    alerts.checkAndAlert('TO_EMAIL', [{ 'coolingType': 'HI_ACTIVE_COOLING' }], -1)
+    expect(sendToEmail.calledOnce)
+  });
+  it('infers a value based on classification of coolingtype MED_ACTIVE_COOLING', () => {
+    alerts.checkAndAlert('TO_EMAIL', [{ 'coolingType': 'MED_ACTIVE_COOLING' }], 41)
+    expect(sendToEmail.calledOnce)
+  });
 });
